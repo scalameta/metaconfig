d8a05f8ff0645a492d40f692abe310c863ac62f3
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-04-22 18:03:33 +0200

    Add ConfDynamic.
    This allows users to dig into deeply nested object without problem.

--- metaconfig-core/shared/src/main/scala/metaconfig/ConfError.scala
+++ metaconfig-core/shared/src/main/scala/metaconfig/ConfError.scala
@@ -4,6 +4,9 @@ import scala.meta.internal.inputs._
 import scala.meta.inputs.Position
 import scala.compat.Platform.EOL
 
+// TODO(olafur) use richer "Message" data type instead of String to support
+// - exceptions with full stack trace
+// - positions
 sealed abstract class ConfError(val msg: String) extends Serializable { self =>
   def extra: List[String] = Nil
   override def toString: String =
@@ -49,17 +52,27 @@ object ConfError {
 
   def msg(message: String): ConfError =
     new ConfError(message) {}
+  def exception(e: Throwable, stackSize: Int = 10): ConfError =
+    msg(s"""$e
+           |${e.getStackTrace.take(stackSize).mkString("\n")}""".stripMargin)
   def fileDoesNotExist(path: String): ConfError =
-    new ConfError(s"File $path does not exist.") {}
-  def parseError(position: Position, msg: String): ConfError =
-    new ConfError(position.formatMessage("parseerror", msg)) {}
-
+    msg(s"File $path does not exist.")
+  def parseError(position: Position, message: String): ConfError =
+    msg(position.formatMessage("parseerror", message))
   def typeMismatch(expected: String, obtained: Conf): ConfError =
-    new ConfError(
+    msg(
       s"""Type mismatch;
          |  found    : ${obtained.kind} (value: $obtained)
          |  expected : $expected""".stripMargin
-    ) {}
+    )
+  def missingField(obj: Conf.Obj, field: String): ConfError = {
+    val closestField =
+      if (obj.values.isEmpty) ""
+      else obj.keys.sorted.minBy(levenshtein(field))
+    msg(
+      s"Object $obj has no field '$field'. " +
+        s"Did you mean '$closestField' instead?")
+  }
 
   // TOOD(olafur) levenshtein
   def invalidFields(invalid: Iterable[String],
@@ -72,5 +85,23 @@ object ConfError {
   def apply(errors: Seq[ConfError]): Option[ConfError] = {
     if (errors.isEmpty) None
     else Some(errors.foldLeft(empty)(_ combine _))
+  }
+
+  /** Levenshtein distance. Implementation based on Wikipedia's algorithm. */
+  private def levenshtein(s1: String)(s2: String): Int = {
+    val dist = Array.tabulate(s2.length + 1, s1.length + 1) { (j, i) =>
+      if (j == 0) i else if (i == 0) j else 0
+    }
+
+    for (j <- 1 to s2.length; i <- 1 to s1.length)
+      dist(j)(i) =
+        if (s2(j - 1) == s1(i - 1))
+          dist(j - 1)(i - 1)
+        else
+          dist(j - 1)(i)
+            .min(dist(j)(i - 1))
+            .min(dist(j - 1)(i - 1)) + 1
+
+    dist(s2.length)(s1.length)
   }
 }
