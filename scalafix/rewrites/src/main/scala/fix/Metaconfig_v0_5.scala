f6571012aeaac44af587d523f30e99d043a3425c
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-06-12 10:01:44 +0200

    WIP

--- scalafix/rewrites/src/main/scala/fix/Metaconfig_v0_5.scala
+++ scalafix/rewrites/src/main/scala/fix/Metaconfig_v0_5.scala
@@ -3,7 +3,7 @@ package fix
 import scalafix._
 import scala.meta._
 
-case class Metaconfig_v0_5(mirror: Mirror) extends SemanticRewrite(mirror) {
+case object Metaconfig_v0_5 extends Rewrite {
   def rewrite(ctx: RewriteCtx): Patch = {
     ctx.reporter.info(ctx.tree.syntax)
     ctx.reporter.info(ctx.tree.structure)
