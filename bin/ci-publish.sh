079b3fe51d675711ccd3e15294b68a7aed9cb5a0
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-07-15 12:32:40 +0200

    Setup travis to publish on tag push

---
+++ bin/ci-publish.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eu
+
+if [ "$TRAVIS_SECURE_ENV_VARS" == true ]; then
+  git log | head -n 20
+  echo "$PGP_SECRET" | base64 --decode | gpg --import
+  if [ -n "$TRAVIS_TAG" ]; then
+    sbt "sonatypeOpen metaconfig-$TRAVIS_TAG" "very publishSigned" sonatypeReleaseAll
+  else
+    echo "Merge into master, no tag push."
+  fi
+else
+  echo "Skipping publish, branch=$TRAVIS_BRANCH"
+fi
