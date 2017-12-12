f3716360d26abb08565857ccd646bbdc0392a669
Guillaume Massé <masgui@gmail.com>
2017-12-12 18:00:40 +0100

    Simplify test case

--- metaconfig-core/shared/src/test/scala/metaconfig/ConfOptionTest.scala
+++ metaconfig-core/shared/src/test/scala/metaconfig/ConfOptionTest.scala
@@ -1,77 +1,30 @@
 package metaconfig
 
-import org.scalameta.logger
 import org.scalatest.FunSuite
 
-object CustomMessage {
-  def decoder[T](field: String)(implicit decoder: ConfDecoder[T]): ConfDecoder[CustomMessage[T]] =
-    ConfDecoder.instance[CustomMessage[T]] {
+case class Foo(a: Option[String])
+object Foo {
+  implicit val reader: ConfDecoder[Foo] =
+    ConfDecoder.instance[Foo] {
       case obj: Conf.Obj =>
-        (obj.get[T](field) |@| 
-          obj.getOption[String]("message")).map{ case (value, message) =>
-          CustomMessage(value, message)
-        }
-      case raw => 
-        decoder.read(raw).map(value => CustomMessage(value, None))
+        obj.getOption[String]("a").map(Foo(_))
     }
-}
-case class CustomMessage[T](value: T, message: Option[String])
-case class DisableConfig(symbols: List[CustomMessage[String]] = Nil) {
-  implicit class XtensionConfScalafix(conf: Conf) {
-    def getField[T: ConfDecoder](e: sourcecode.Text[T]): Configured[T] =
-      conf.getOrElse(e.source)(e.value)
-  }
-
-  implicit val customMessageReader: ConfDecoder[CustomMessage[String]] =
-    CustomMessage.decoder(field = "symbol")
-
-  implicit val reader: ConfDecoder[DisableConfig] =
-    ConfDecoder.instanceF[DisableConfig](
-      _.getField(symbols).map(DisableConfig(_))
-    )
-}
-
-object DisableConfig {
-  val default = DisableConfig()
-  implicit val reader: ConfDecoder[DisableConfig] = default.reader
 }
 
 class ConfOptionTest extends FunSuite {
-  def decode[T](conf: Conf)(implicit decoder: ConfDecoder[T]): T = 
-    decoder.read(conf).get
-
   import Conf._
 
-  test("all") {
-    val conf = Obj(
-      "symbols" -> Lst(
-        Str("scala.Any.asInstanceOf"),
-        Obj(
-          "symbol" -> Str("scala.Option.get")
-        ),
-        Obj(
-          "symbol" -> Str("scala.List.get"),
-          "message" -> Str("You should not use List.get")
-        )
-      )
-    )
-    val obtained = decode[DisableConfig](conf)
-    val expected = DisableConfig(
-      symbols = List(
-        CustomMessage(
-          value = "scala.Any.asInstanceOf",
-          message = None
-        ),
-        CustomMessage(
-          value = "scala.Option.get",
-          message = None
-        ),
-        CustomMessage(
-          value = "scala.List.get",
-          message = Some("You should not use List.get")
-        )
-      )
-    )
+  test("simple") {
+    val conf = Obj("a" -> Str("b"))
+    val obtained = conf.as[Foo].get
+    val expected = Foo(Some("b"))
+    assert(obtained == expected)
+  }
+
+  test("missing") {
+    val conf = Obj()
+    val obtained = conf.as[Foo].get
+    val expected = Foo(None)
     assert(obtained == expected)
   }
 }
