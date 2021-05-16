fa3897cad0e74e69c4bcc0e6b8fc5c6574c2d095
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-05-16 10:44:55 -0700

    CanBuilderFromDecoder: extract helper from list()

--- metaconfig-core/shared/src/main/scala/metaconfig/internal/CanBuildFromDecoder.scala
+++ metaconfig-core/shared/src/main/scala/metaconfig/internal/CanBuildFromDecoder.scala
@@ -5,6 +5,7 @@ import scala.reflect.ClassTag
 import metaconfig.Conf
 import metaconfig.ConfDecoder
 import metaconfig.ConfError
+import metaconfig.Configured
 import metaconfig.Configured.NotOk
 import metaconfig.Configured.Ok
 
@@ -37,19 +38,27 @@ object CanBuildFromDecoder {
       s"List[${classTag.runtimeClass.getName}]"
     ) {
       case Conf.Lst(values) =>
-        val successB = factory.newBuilder
-        val errorB = List.newBuilder[ConfError]
-        successB.sizeHint(values.length)
-        values.foreach { value =>
-          ev.read(value) match {
-            case NotOk(e) => errorB += e
-            case Ok(e) => successB += e
-          }
-        }
-        ConfError(errorB.result()) match {
-          case Some(x) => NotOk(x)
-          case None => Ok(successB.result())
-        }
+        build(values, ev, factory)(identity, (_, x) => x)
     }
+
+  private def build[A, B, C, Coll](
+      values: List[A],
+      ev: ConfDecoder[B],
+      factory: Factory[C, Coll]
+  )(a2conf: A => Conf, ab2c: (A, B) => C): Configured[Coll] = {
+    val successB = factory.newBuilder
+    val errorB = List.newBuilder[ConfError]
+    successB.sizeHint(values.length)
+    values.foreach { value =>
+      ev.read(a2conf(value)) match {
+        case NotOk(e) => errorB += e
+        case Ok(decoded) => successB += ab2c(value, decoded)
+      }
+    }
+    ConfError(errorB.result()) match {
+      case Some(x) => NotOk(x)
+      case None => Ok(successB.result())
+    }
+  }
 
 }
