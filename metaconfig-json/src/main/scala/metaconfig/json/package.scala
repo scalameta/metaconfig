7db754e19610b9317d41624f3adeebf42f1f42f2
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2021-05-17 12:16:40 -0700

    Use single abstract method available in scala 2.12

--- metaconfig-json/src/main/scala/metaconfig/json/package.scala
+++ metaconfig-json/src/main/scala/metaconfig/json/package.scala
@@ -5,15 +5,13 @@ import scala.util.control.NonFatal
 
 package object json {
 
-  implicit val parser: MetaconfigParser = new MetaconfigParser {
-    override def fromInput(input: Input): Configured[Conf] = {
-      try {
-        val js = JsonConverter.fromInput(input)
-        Configured.ok(JsonConverter.toConf(js))
-      } catch {
-        case NonFatal(e) =>
-          Configured.exception(e)
-      }
+  implicit val parser: MetaconfigParser = (input: Input) => {
+    try {
+      val js = JsonConverter.fromInput(input)
+      Configured.ok(JsonConverter.toConf(js))
+    } catch {
+      case NonFatal(e) =>
+        Configured.exception(e)
     }
   }
 
