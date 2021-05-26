8b626385774e203c8c38f1ca92f5cecda71a76f2
Brice Jaglin <bjaglin@teads.tv>
2021-05-26 23:55:50 +0200

    align SConfig2ClassSuite with TypesafeConfig2ClassSuite

--- metaconfig-tests/jvm/src/test/scala/metaconfig/sconfig/SConfig2ClassSuite.scala
+++ metaconfig-tests/jvm/src/test/scala/metaconfig/sconfig/SConfig2ClassSuite.scala
@@ -3,7 +3,7 @@ package metaconfig.sconfig
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
-import metaconfig.Conf
+import metaconfig.{Conf, Position}
 
 class SConfig2ClassSuite extends munit.FunSuite {
   test("basic") {
@@ -49,5 +49,55 @@ class SConfig2ClassSuite extends munit.FunSuite {
       )
     )
     assertEquals(obtained, expected)
+  }
+
+  test("include") {
+    val dir = Files.createTempDirectory("include")
+
+    val main = dir.resolve("main.conf")
+    Files.write(
+      main,
+      """|a = [ 1 ]
+         |include "included.conf"
+         |c = bar
+         |""".stripMargin.getBytes()
+    )
+
+    val included = dir.resolve("included.conf")
+    Files.write(
+      included,
+      s"""|a = $${a} [
+          |  "2",
+          |  3,
+          |  true
+          |]
+          |b = foo
+          |""".stripMargin.getBytes()
+    )
+
+    val obtained = SConfig2Class.gimmeConfFromFile(main.toFile).get
+    val expected: Conf = Conf.Obj(
+      "a" -> Conf.Lst(
+        Conf.Num(1),
+        Conf.Str("2"),
+        Conf.Num(3),
+        Conf.Bool(true)
+      ),
+      "b" -> Conf.Str("foo"),
+      "c" -> Conf.Str("bar")
+    )
+    assertEquals(obtained, expected)
+
+    val obtainedObj = obtained.asInstanceOf[Conf.Obj]
+
+    // declaration spread between main file and included file -> unknown position
+    val aPos = obtainedObj.field("a").get.pos
+    assertEquals(aPos, Position.None: Position)
+
+    val bPos = obtainedObj.field("b").get.pos
+    assertEquals(bPos.lineContent, "b = foo")
+
+    val cPos = obtainedObj.field("c").get.pos
+    assertEquals(cPos.lineContent, "c = bar")
   }
 }
